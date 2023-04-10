1f9313f2b5f3ec60145f87faf5759bf9c68728e6
dongYoun2 <dlehddbs6425@gmail.com>
2023-04-11 08:05:41 +0900

    a4: code implementation. sanity check 1e passed.

--- a4/nmt_model.py
+++ a4/nmt_model.py
@@ -255,6 +255,17 @@ class NMT(nn.Module):
         ###     Tensor Stacking:
         ###         https://pytorch.org/docs/stable/torch.html#torch.stack
 
+        enc_hiddens_proj = self.att_projection(enc_hiddens) # (b, src_len, h)
+        Y = self.model_embeddings.target(target_padded) # (tgt_len, b, e)
+
+        for Y_t in torch.split(Y, 1):
+            Y_t = torch.squeeze(Y_t, 0) # (b, e)
+            Ybar_t = torch.cat((Y_t, o_prev), dim=1)    # (b, (e+h))
+            dec_state, o_t, _ = self.step(Ybar_t, dec_state, enc_hiddens, enc_hiddens_proj, enc_masks)
+            combined_outputs.append(o_t)
+            o_prev = o_t
+        
+        combined_outputs = torch.stack(combined_outputs)    # (tgt_len, b, h)
 
         ### END YOUR CODE
 
